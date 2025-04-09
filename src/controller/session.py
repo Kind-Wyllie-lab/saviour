b3a1fed156a6b86d0722936459e491b3eeb42262
andrewsg3 <andrewscottgeorge@outlook.com>
2025-04-09 13:04:11 +1000

    fix: session manager just generates session id. no point storing it as there will be multiple sessions active and managed by the controller.

--- src/controller/session.py
+++ src/controller/session.py
@@ -4,20 +4,12 @@ from datetime import datetime
 
 class SessionManager:
     def __init__(self):
-        self.session_id = None # A dictionary containing the current session data
+        pass
 
-    def start_session(self, module_id=None):
+    def generate_session_id(self, module_id=None):
         """Start a new session for a module"""
         timestamp = datetime.now().strftime("%Y%m%d %H%M%S")
-        self.session_id = f"REC_{timestamp}_{module_id}" # Generate a new session ID
-        return self.session_id
+        return f"REC_{timestamp}_{module_id}" # Generate a new session ID
 
-    def end_session(self):
-        """End the current session"""
-        self.session_id = None
-    
-    def get_session_id(self):
-        """Get the current session ID"""
-        return self.session_id
 
 

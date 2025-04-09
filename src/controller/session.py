ed926ca8208d23f759342feed2174f277985b37c
andrewsg3 <andrewscottgeorge@outlook.com>
2025-04-09 13:10:34 +1000

    feat: usage of SessionManager class within controller program to generate a session id. TODO: apply this to a start recording command.

--- src/controller/session.py
+++ src/controller/session.py
@@ -6,7 +6,7 @@ class SessionManager:
     def __init__(self):
         pass
 
-    def generate_session_id(self, module_id=None):
+    def generate_session_id(self, module_id="unknown"):
         """Start a new session for a module"""
         timestamp = datetime.now().strftime("%Y%m%d %H%M%S")
         return f"REC_{timestamp}_{module_id}" # Generate a new session ID

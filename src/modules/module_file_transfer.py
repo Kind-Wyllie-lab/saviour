44d94ed4d15f5b965113949361e6df54d7c74a81
Andrew SG <andrewscottgeorge@outlook.com>
2025-05-09 11:37:57 +0100

    feat: file transfer test file includes test case for submitting file over local network.

--- src/modules/module_file_transfer.py
+++ src/modules/module_file_transfer.py
@@ -9,34 +9,51 @@ class ModuleFileTransfer:
         self.session = aiohttp.ClientSession()
         self.logger = logger # Use the modules logger
 
+    async def _ensure_session(self):
+        """Private method to ensure we have a session, create if needed"""
+        if self.session is None:
+            self.session = aiohttp.ClientSession()
+
     async def send_file(self, filepath: str):
         """Send a file to the controller"""
         try:
+            self.logger.debug(f"Sending file to controller: {filepath}")
+            
+            # Ensure we have a session
+            await self._ensure_session()
+            self.logger.debug(f"Session: {self.session}")
+            
             # Format the file for transfer
             data = aiohttp.FormData()
             data.add_field('file',
-                open('filepath', 'rb'),
+                open(filepath, 'rb'),
                 filename=os.path.basename(filepath),
                 content_type='video/mp4')
+            self.logger.debug(f"Data: {data}")
             
             # Send the file to controller
             async with self.session.post(
                 f'http://{self.controller_ip}:8080/upload', 
                 data=data
                 ) as response:
-                print(await response.text())
-        
-            # TODO: Check if the file was uploaded successfully
+                print(f"Response: {response}")
+                if response.status == 200:
+                    self.logger.debug(f"File uploaded successfully")
+                    return True
+                else:
+                    self.logger.error(f"File upload failed with status: {response.status}")
+                    return False
 
-            # TODO: Log the file transfer
-
-            # TODO: If successful, delete the file
 
         except Exception as e:
             self.logger.error(f"Error sending file: {e}")
             return False
     
-
+    async def close(self):
+        """Close the session if it exists"""
+        if self.session:
+            await self.session.close()
+            self.session = None
 
 
 

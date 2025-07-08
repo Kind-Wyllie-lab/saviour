7de36d0fb3e5042b6ae06730eab6a5653389cc7c
Andrew SG <andrewscottgeorge@outlook.com>
2025-07-08 14:07:57 +0100

    feat: list commands route

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/pi/Desktop/habitat/env
+set -gx VIRTUAL_ENV /home/pi/Desktop/apa/src/habitat/env
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH

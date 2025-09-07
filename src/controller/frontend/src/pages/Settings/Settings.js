e5e466fd23116b822974f014661546b338bb84da
Andrew SG <andrewscottgeorge@outlook.com>
2025-09-07 13:36:21 +0100

    feat:config card

--- src/controller/frontend/src/pages/Settings/Settings.js
+++ src/controller/frontend/src/pages/Settings/Settings.js
@@ -4,22 +4,25 @@ import socket from "../../socket";
 
 // Styling and components
 import "./Settings.css"; // optional, if you want page-specific styles
-import ModuleCard from "../../components/ModuleCard/ModuleCard";
+import ConfigCard from "../../components/ConfigCard/ConfigCard";
 
 function Settings() {
-  const [modules, setModules] = useState({}); 
+  // const [modules, setModules] = useState({}); 
+  const [moduleConfigs, setModuleConfigs] = useState({}); // State to hold module configurations
 
   useEffect(() => { // React hook that runs "side effects"; functionality outside of rendering.
-    console.log("Emitting get_modules");
-    socket.emit("get_modules"); // Ask backend for modules
+    console.log("Emitting get_module_configs");
+    socket.emit("get_module_configs"); // Ask backend for module configurations
 
-    socket.on("modules_update", (data) => {
-      setModules(data);
-    }); // When modules received from backend
+    socket.on("module_configs_update", (data) => {
+      setModuleConfigs(data);
+      console.log("Received module configs:", data);
+      console.log(Object.entries(data.module_configs || {}));
+    }); // When module configs received from backend
 
     // Cleanup listener on unmount
     return () => {
-      socket.off("modules_update");
+      socket.off("module_configs_update");
     };
   }, []); // [] Dependency array indicates function should run once after first render (componentDidMount)
 
@@ -28,8 +31,8 @@ function Settings() {
     <main className="settings">
       <h2>Modules to update settings for</h2>
       <div className="module-grid">
-        {Object.entries(modules).map(([id, module]) => (
-          <ModuleCard key={id} module={module} />
+        {Object.entries(moduleConfigs.module_configs || {}).map(([id, moduleConfig]) => (
+          <ConfigCard key={id} id={id} config={moduleConfig} />
         ))}
       </div>
     </main>

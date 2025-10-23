e6aebb16b21e3024f2a5c71a44d34bc750fa54a4
Andrew SG <andrewscottgeorge@outlook.com>
2025-10-23 15:13:22 +0100

    fix: camera takes recording filetype from config

--- src/modules/examples/camera/camera_module.py
+++ src/modules/examples/camera/camera_module.py
@@ -42,6 +42,9 @@ class CameraModule(Module):
     def __init__(self, module_type="camera"):        
         # Call the parent class constructor
         super().__init__(module_type)
+
+        # Update config 
+        self.config.load_module_config("camera_config.json")
     
         # Initialize camera
         self.picam2 = Picamera2()
@@ -71,9 +74,6 @@ class CameraModule(Module):
         self.is_recording = False
         self.is_streaming = False
         self.frame_times = []  # For storing frame timestamps
-
-        # Update config 
-        self.config.load_module_config("camera_config.json")
 
         # Set up camera-specific callbacks for the command handler
         self.camera_callbacks = {
@@ -171,7 +171,7 @@ class CameraModule(Module):
     def _start_recording(self):
         """Implement camera-specific recording functionality"""
         self.logger.info("Executing camera specific recording functionality...")
-        filename = f"{self.current_filename}.{self.recording_filetype}"
+        filename = f"{self.current_filename}.{self.config.get('recording.recording_filetype')}"
         self.add_session_file(filename)
         try:
             # Start the camera if not already running

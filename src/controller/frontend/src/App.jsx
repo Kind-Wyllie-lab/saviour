bfd6c98963dfebf07cce06a755d93111d3f1658b
Andrew SG <andrewscottgeorge@outlook.com>
2025-11-11 09:33:58 +0000

    refactor: APADashboard uses current practices from Dashboard.jsx

--- src/controller/frontend/src/App.jsx
+++ src/controller/frontend/src/App.jsx
@@ -4,7 +4,7 @@ import React,  { useEffect, useState } from "react";
 
 import Header from "./components/Header/Header";
 import { Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard/Dashboard";
+import APADashboard from "./pages/APADashboard/APADashboard";
 import Settings from "./pages/Settings/Settings";
 import Debug from "./pages/Debug/Debug";
 
@@ -35,7 +35,7 @@ function App() {
     <div>
       <Header />
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        <Route path="/" element={<APADashboard />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/debug" element={<Debug />} />
       </Routes>

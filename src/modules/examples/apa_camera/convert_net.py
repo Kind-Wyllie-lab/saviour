18895f8e51dbab0bad85b1e729f007a8614757cb
Andrew SG <andrewscottgeorge@outlook.com>
2025-11-13 15:20:57 +0000

    refactor: backup

--- src/modules/examples/apa_camera/convert_net.py
+++ src/modules/examples/apa_camera/convert_net.py
@@ -1,10 +1,14 @@
 from ultralytics import YOLO
 
+PATH = "ratnet.pt"
+
 # Load a YOLO11n PyTorch model
-model = YOLO("yolo11n.pt")
+model = YOLO(PATH)
 
 # Export the model
-model.export(format="imx", int8=False)  # exports with PTQ quantization by default
+# model.export(format="imx", int8=False)  # exports with PTQ quantization by default
+
+model.export(format="ncnn")
 
 # Load the exported model
 # imx_model = YOLO("yolo11n_imx_model")

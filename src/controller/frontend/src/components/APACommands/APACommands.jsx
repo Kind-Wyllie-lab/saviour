6f39e47bb1b42ea6cbd65e705798cd5d44b93a64
Andrew SG <andrewscottgeorge@outlook.com>
2025-12-05 17:53:39 +0000

    refactor: pass moduleList through to fullscreen video component so it can spawn an APACommand component

--- src/controller/frontend/src/components/APACommands/APACommands.jsx
+++ src/controller/frontend/src/components/APACommands/APACommands.jsx
@@ -6,9 +6,11 @@ import "./APACommands.css";
 
 function APACommands( {modules} ) {
     const [shockState, setShockState] = useState(null); // Will be updated by socketio event - indicates whether grid live, shock being delivered etc.
+    const [arduinoState, setArduinoState] = useState(null); // State object from the apa rig
+
     const apaModule = modules.filter((m) => m.type === "apa_arduino")[0];
-    apaModule ? console.log("APA Module Connected") : console.log("No APA module connected");
-    apaModule ? console.log(apaModule.ip) : null;
+    // apaModule ? console.log("APA Module Connected") : console.log("No APA module connected");
+    // apaModule ? console.log(apaModule.ip) : null;
 
     useEffect(() => {
         // Handle shock state changes
@@ -20,8 +22,16 @@ function APACommands( {modules} ) {
             setShockState("Stopped shocking");
         }
 
+        function onArduinoState(data) {
+            setArduinoState(data.state);
+        }
+
         socket.on('shock_started_being_delivered', onShockStartBeingDelivered);
         socket.on('shock_stopped_being_delivered', onShockStopBeingDelivered);
+        socket.on('arduino_state', (data) => {
+            // console.log(data);
+            onArduinoState(data);
+        });
 
         return () => {
             socket.off('shock_started_being_delivered', onShockStartBeingDelivered);
@@ -72,11 +82,38 @@ function APACommands( {modules} ) {
     return (
         <div className="apa-commands">
             <h2>APA Commands</h2>
-            {shockState ? (
+            {/* {shockState ? (
                 <h3>Shock state: {shockState}</h3>
             ) : (
                 <></>
-            )}
+            )} */}
+            <div className="apa-state">
+                {arduinoState? (
+                    <div>
+                        {arduinoState.shock_activated? (
+                            <p>Shock Sequence Active</p>
+                        ): (
+                            <p>Shock Sequence Inactive</p>
+                        )}
+                        {arduinoState.grid_live? (
+                            <p>Grid Live</p>
+                        ) : (
+                            <p>Grid Not Live</p>
+                        )}
+                        <p>Attempted Shocks {arduinoState.attempted_shocks}</p>
+                        <p>Attemped Shocks (from Arduino) {arduinoState.attempted_shocks}</p>
+                        <p>Delivered Shocks {arduinoState.delivered_shocks}</p>
+                        <p>Table RPM {arduinoState.rpm}</p>
+                        {arduinoState.rotating? (
+                            <p>Rotating</p>
+                        ) : (
+                            <p>Stationary</p>
+                        )}
+                    </div>
+                ) : (
+                    <p>Arduino not reporting state</p>
+                )}
+            </div>
             <div className = "apa-command-buttons">
                 <button
                     className="activate-shock"

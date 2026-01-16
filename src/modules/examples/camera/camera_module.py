ee0718d76485f4b7227550554a0ea17857f0faee
Andrew SG <andrewscottgeorge@outlook.com>
2026-01-16 12:44:01 +0000

    feat: prepend module name to livestream timestamp

--- src/modules/examples/camera/camera_module.py
+++ src/modules/examples/camera/camera_module.py
@@ -414,6 +414,7 @@ class CameraModule(Module):
                 self.logger.warning("No data returned by frame wall clock")
                 return
             timestamp = str(datetime.datetime.fromtimestamp(timestamp / 1e9, tz=datetime.timezone.utc)) # Format timestamp. Example: 2026-01-08 15:25:01.125786+00:00
+            timestamp = f"{self.api.get_module_name()} {timestamp}"
 
             # Modify main stream - used for recording.
             with MappedArray(req, 'main') as m:

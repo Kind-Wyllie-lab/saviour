f7f1eb35477eee9a9ef67ede8c0a8b75374a3dc3
Andrew SG <andrewscottgeorge@outlook.com>
2026-03-13 15:10:38 +0000

    refactor: microphone config card + spectrogram stream

--- src/controller/frontend/src/basic/components/ConfigCard/ConfigCard.jsx
+++ src/controller/frontend/src/basic/components/ConfigCard/ConfigCard.jsx
@@ -1,8 +1,9 @@
 // Simple element that builds the correct config card based on id / module object
 import './ConfigCard.css';
-import GenericConfigCard from "./GenericConfigCard/GenericConfigCard";
-import CameraConfigCard from "./CameraConfigCard/CameraConfigCard";
-import ControllerConfigCard from './ControllerConfigCard/ControllerConfigCard';
+import GenericConfigCard      from "./GenericConfigCard/GenericConfigCard";
+import CameraConfigCard       from "./CameraConfigCard/CameraConfigCard";
+import MicrophoneConfigCard   from "./MicrophoneConfigCard/MicrophoneConfigCard";
+import ControllerConfigCard   from './ControllerConfigCard/ControllerConfigCard';
 
 function ConfigCard({ id, module, clipboard, onCopy }) {
   if (id === "controller") {
@@ -13,6 +14,9 @@ function ConfigCard({ id, module, clipboard, onCopy }) {
   }
   if (module.type?.includes("camera")) {
     return <CameraConfigCard id={id} module={module} clipboard={clipboard} onCopy={onCopy} />;
+  }
+  if (module.type?.includes("microphone")) {
+    return <MicrophoneConfigCard id={id} module={module} clipboard={clipboard} onCopy={onCopy} />;
   }
   return <GenericConfigCard id={id} module={module} clipboard={clipboard} onCopy={onCopy} />;
 }
